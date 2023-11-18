9d694c62faff9f630aeddd0ee6c21a2fbe263265
Ignacio H <55958716+kiltro-dev@users.noreply.github.com>
2023-11-18 20:10:15 -0300

    typing and iife

---
+++ src/03-typing.ts
@@ -0,0 +1,10 @@
+(() => {
+  let myProductName = 'Product 1';
+  let myProductPrice = 123;
+
+  myProductName = 'change';
+  myProductName.toLowerCase();
+  myProductPrice.toFixed();
+
+  const myProductsStock = 1000;
+})();
